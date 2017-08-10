10af963337c43a4f2fcf6caff0ff1c5095c6e364
JRiascos1995 <jamin.r.m@hotmail.com>
2017-08-10 14:50:06 -0500

    inicio

--- pc.c
+++ pc.c
@@ -5,4 +5,13 @@
 #include <pthread.h>
 #include <string.h>
 
+int main(int argc, char ** argv){
+	
+	if (argc < 6) {
 
+		printf("./pc <num_hilos> <tiempo_prod> <num_hilos_cons> <tiempo_cons> <tam_cola>
+<total_items> \n");
+
+		exit(1);
+
+}
