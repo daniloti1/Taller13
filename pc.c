eee9a0df91de6b51d4f01f524f4c0e201915e096
JRiascos1995 <jamin.r.m@hotmail.com>
2017-08-10 15:01:05 -0500

    hilos

--- pc.c
+++ pc.c
@@ -22,5 +22,25 @@ int main(int argc, char ** argv){
 	int tiempoProduccion = argv[2];
 
 	printf("");
+	pthread_t * listaConsumidores = (pthread_t *) malloc(sizeof(pthread_t)*hilos);
+	pthread_t * listaProductores = (pthread_t *) malloc(sizeof(pthread_t)*hilos);
 
+	for (int j = 0; j < productores; j ++) {		
+
+		int statusHilo = pthread_create(&listaConsumidores[j], NULL, funcion, (void *)estructura);
+
+
+		if (statusHilo < 0) {
+
+			fprintf(stderr, "Error al crear el hilo\n");
+
+			exit(-1);	
+
+		}
+
+
+		contador++;
+
+	}
+	
 }
