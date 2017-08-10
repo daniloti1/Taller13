f3397d6a65798c7fce8a7ec81bcb570e8f61bec4
tugfa <tugfa@gmail.com>
2017-08-10 15:02:44 -0500

    avance2

--- pc.c
+++ pc.c
@@ -15,15 +15,25 @@ int main(int argc, char ** argv){
 		exit(1);
 	}
 
+	pthread_t * listaConsumidores = (pthread_t *) malloc(sizeof(pthread_t)*hilos);
+	pthread_t * listaProductores = (pthread_t *) malloc(sizeof(pthread_t)*hilos);
+
 	int productores = argv[1];
 	int consumidores = argv[3];
 	int tamCola = argv[5];
-	int tiempoConsumo = argv[4];
-	int tiempoProduccion = argv[2];
+	double tiempoConsumo = argv[4];
+	double tiempoProduccion = argv[2];
+	int items = argv[6];
 
-	printf("");
-	pthread_t * listaConsumidores = (pthread_t *) malloc(sizeof(pthread_t)*hilos);
-	pthread_t * listaProductores = (pthread_t *) malloc(sizeof(pthread_t)*hilos);
+	printf("Numero de productores: %d\n", productores);
+	printf("Numero de consumidores: %d\n", consumidores);
+	printf("Tamaño de cola: %d\n", tamCola);
+	printf("Tiempo de consumo: %d\n", tiempoConsumo);
+	printf("Tiempo de produccion: %d\n", tiempoProduccion);
+	printf("Total de items a producir: %d\n", items);	
+
+	int cola = 0;
+	
 
 	for (int j = 0; j < productores; j ++) {		
 
